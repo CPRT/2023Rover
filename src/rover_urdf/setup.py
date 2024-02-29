8a464b40d2f3964faebf3faf04beab40c20270a2
Chris <christopherrusu1@gmail.com>
2024-02-28 19:04:46 -0500

    add rover urdf

---
+++ src/rover_urdf/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'rover_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='chris',
+    maintainer_email='christopherrusu1@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
