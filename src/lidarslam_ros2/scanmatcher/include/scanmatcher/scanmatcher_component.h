778ce03a678f2589aa29d1f65fabd7f224d3eefb
Connor Needham <connor.needham2015@gmail.com>
2024-03-27 21:58:17 -0400

    change config slam

--- src/lidarslam_ros2/scanmatcher/include/scanmatcher/scanmatcher_component.h
+++ src/lidarslam_ros2/scanmatcher/include/scanmatcher/scanmatcher_component.h
@@ -82,13 +82,12 @@ public:
 
 private:
 
-
-    const std::array<double, 36> kCovariance ({0.1, 0, 0, 0, 0, 0,
-                                               0, 0.1, 0, 0, 0, 0,
-                                               0, 0, 0.1, 0, 0, 0,
-                                               0, 0, 0, 0.1, 0, 0,
-                                               0, 0, 0, 0, 0.1, 0,
-                                               0, 0, 0, 0, 0, 0.1});
+    static constexpr std::array<double, 36> kCovariance = {1, 0, 0, 0, 0, 0,
+                                                           0, 1, 0, 0, 0, 0,
+                                                           0, 0, 1, 0, 0, 0,
+                                                           0, 0, 0, 1, 0, 0,
+                                                           0, 0, 0, 0, 1, 0,
+                                                           0, 0, 0, 0, 0, 1};
     rclcpp::Clock clock_;
     tf2_ros::Buffer tfbuffer_;
     tf2_ros::TransformListener listener_;
