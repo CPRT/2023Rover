0ed428a92901ab132756c6f44fee8cb1df5867f5
lead <softlead@curovers.ca>
2024-04-29 19:52:54 -0400

    costmap params

--- src/ros2_rover/rover_navigation/launch/navigation.launch.py
+++ src/ros2_rover/rover_navigation/launch/navigation.launch.py
@@ -29,6 +29,7 @@ from nav2_common.launch import RewrittenYaml
 def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory("nav2_bringup")
+    pkg_dir = get_package_share_directory("rover_navigation")
 
     namespace = LaunchConfiguration("namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
@@ -87,7 +88,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         "params_file",
-        default_value=os.path.join(bringup_dir, "params", "nav2_params.yaml"),
+        default_value=os.path.join(pkg_dir, "params", "nav2.yaml"),
         description="Full path to the ROS2 parameters file to use for all launched nodes")
 
     declare_autostart_cmd = DeclareLaunchArgument(
