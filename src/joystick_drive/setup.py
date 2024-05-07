7150d14c9b549be075b3a4564dd0b4c2606906a1
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-05-07 19:31:46 -0400

    setup

--- src/joystick_drive/setup.py
+++ src/joystick_drive/setup.py
@@ -1,15 +1,17 @@
-from setuptools import find_packages, setup
-
+from setuptools import setup
+import os
+from glob import glob
 package_name = 'joystick_drive'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joystick = joystick_drive/joystick_drive:main'
+            'joystick = joystick_drive.joystick_drive:main'
         ],
     },
 )
