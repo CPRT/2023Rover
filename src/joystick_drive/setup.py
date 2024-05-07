291732a671803dad3ca4c12ba670ddac0b36b737
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-05-07 18:54:50 -0400

    Joystick attempt #1 (didn't work)

---
+++ src/joystick_drive/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'joystick_drive'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='will',
+    maintainer_email='51888361+MaybeWilli@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
