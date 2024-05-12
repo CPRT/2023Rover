b511b8927b1427d05cc13bc2af19cbc568f7035d
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-05-12 13:50:27 -0400

    Joystick things

---
+++ src/joystick_publisher/src/joystick_publisher.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class JoystickReader : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world joystick_publisher package\n");
+  return 0;
+}
