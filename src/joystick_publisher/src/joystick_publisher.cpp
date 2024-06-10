58ffa37d827cdcd0e81c8325e7dc07b59dce60f0
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-06-10 13:53:42 -0400

    Revert "Uses header files"
    This reverts commit 4df60a2a3c35af8bef969f15997870e63f8f58c4.

--- src/joystick_publisher/src/joystick_publisher.cpp
+++ src/joystick_publisher/src/joystick_publisher.cpp
@@ -1,4 +1,16 @@
-#include "joystick_publisher.h"
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include <moveit/move_group_interface/move_group_interface.h>
+#include "std_msgs/msg/string.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include "interfaces/msg/arm_cmd.hpp"
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -8,158 +20,167 @@ bool isEqual(interfaces::msg::ArmCmd a, interfaces::msg::ArmCmd b)
   return a == b;
 }
 
-JoystickReader::JoystickReader()
-: Node("JoystickReader")
+class JoystickReader : public rclcpp::Node
 {
-  subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-  "joy", 10, std::bind(&JoystickReader::topic_callback, this, _1));
-  publisher_ = this->create_publisher<interfaces::msg::ArmCmd>("arm_base_commands", 10);
-  timer_ = this->create_wall_timer(
-  600ms, std::bind(&JoystickReader::publish_message, this));//*/
-}
+  public:
+    JoystickReader()
+    : Node("JoystickReader")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "joy", 10, std::bind(&JoystickReader::topic_callback, this, _1));
+      publisher_ = this->create_publisher<interfaces::msg::ArmCmd>("arm_base_commands", 10);
+      timer_ = this->create_wall_timer(
+      600ms, std::bind(&JoystickReader::publish_message, this));//*/
+    }
 
-
-void JoystickReader::topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
-{
-  //RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->axes[1]);
-  interfaces::msg::ArmCmd poseCmd = []{
-		interfaces::msg::ArmCmd msg;
-		msg.pose.position.x = 0;
-		msg.pose.position.y = 0;
-		msg.pose.position.z = 0;
-		msg.pose.orientation.x = 0;
-		msg.pose.orientation.y = 0;
-		msg.pose.orientation.z = 0;
-		msg.pose.orientation.w = 0;
-		msg.speed = 10;
-		msg.estop = false;
-		msg.reset = false;
-		return msg;
-	}();
-  if (msg->axes[0] > 0.4)
-  {
-    poseCmd.pose.position.y = 1;
-  }
-  if (msg->axes[0] < -0.4)
-  {
-    poseCmd.pose.position.y = -1;
-  }
-  if (msg->axes[1] > 0.4)
-  {
-    poseCmd.pose.position.x = 1;
-  }
-  if (msg->axes[1] < -0.4)
-  {
-    poseCmd.pose.position.x = -1;
-  }
-  if (msg->buttons[9] == 1)
-  {
-    poseCmd.pose.position.z = 1;
-  }
-  if (msg->buttons[10] == 1)
-  {
-    poseCmd.pose.position.z = -1;
-  }
-  if (msg->axes[2] > 0.4)
-  {
-    poseCmd.pose.orientation.x = -1;
-  }
-  if (msg->axes[2] < -0.4)
-  {
-    poseCmd.pose.orientation.x = 1;
-  }
-  if (msg->axes[3] > 0.4)
-  {
-    poseCmd.pose.orientation.y = 1;
-  }
-  if (msg->axes[3] < -0.4)
-  {
-    poseCmd.pose.orientation.y = -1;
-  }
-  if (msg->axes[4] == -1)
-  {
-    poseCmd.pose.orientation.z = 1;
-  }
-  if (msg->axes[5] == -1)
-  {
-    poseCmd.pose.orientation.z = -1;
-  }
-  if (msg->buttons[3] == 1)
-  {
-    poseCmd.pose.orientation.w = 1;
-  }
-  if (msg->buttons[1] == 1)
-  {
-    poseCmd = []{
-			interfaces::msg::ArmCmd msg;
-			msg.pose.position.x = 0;
-			msg.pose.position.y = 0;
-			msg.pose.position.z = 0;
-			msg.pose.orientation.x = 0;
-			msg.pose.orientation.y = 0;
-			msg.pose.orientation.z = 0;
-			msg.pose.orientation.w = 0;
-			msg.speed = 10;
-			msg.estop = false;
-			msg.reset = true;
-			return msg;
-		}();
-    //shouldPub = true;
-  }
-  if (!isEqual(poseCmd, oldCmd))
-  {
-    /*RCLCPP_INFO(this->get_logger(), "A pose: %f %f %f %f %f %f %f %f %i %i",
-			oldCmd.pose.position.x,
-			oldCmd.pose.position.y,
-			oldCmd.pose.position.z,
-			oldCmd.pose.orientation.x,
-			oldCmd.pose.orientation.y,
-			oldCmd.pose.orientation.z,
-			oldCmd.pose.orientation.w,
-			oldCmd.speed,
-			int(oldCmd.estop),
-			int(oldCmd.reset));//
-		RCLCPP_INFO(this->get_logger(), "B pose: %f %f %f %f %f %f %f %f %i %i",
-			poseCmd.pose.position.x,
-			poseCmd.pose.position.y,
-			poseCmd.pose.position.z,
-			poseCmd.pose.orientation.x,
-			poseCmd.pose.orientation.y,
-			poseCmd.pose.orientation.z,
-			poseCmd.pose.orientation.w,
-			poseCmd.speed,
-			int(poseCmd.estop),
-			int(poseCmd.reset));//*/
-    oldCmd = poseCmd;
-    shouldPub = true;
-    /*auto message = std_msgs::msg::String();
-    message.data = cmd;
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);*/
-  }
-}
-
-void JoystickReader::publish_message()
-{
-  if (shouldPub)
-  {
-    shouldPub = false;
-    RCLCPP_INFO(this->get_logger(), "Current pose: %f %f %f %f %f %f %f %f %i %i",
-			oldCmd.pose.position.x,
-			oldCmd.pose.position.y,
-			oldCmd.pose.position.z,
-			oldCmd.pose.orientation.x,
-			oldCmd.pose.orientation.y,
-			oldCmd.pose.orientation.z,
-			oldCmd.pose.orientation.w,
-			oldCmd.speed,
-			int(oldCmd.estop),
-			int(oldCmd.reset));//*/
-	  publisher_->publish(oldCmd);
-	}
-}
-
-
+  private:
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Publisher<interfaces::msg::ArmCmd>::SharedPtr publisher_;
+    interfaces::msg::ArmCmd oldCmd;
+    rclcpp::TimerBase::SharedPtr timer_;
+    bool shouldPub = false;
+		
+    void topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+    {
+      //RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->axes[1]);
+      interfaces::msg::ArmCmd poseCmd = []{
+				interfaces::msg::ArmCmd msg;
+				msg.pose.position.x = 0;
+				msg.pose.position.y = 0;
+				msg.pose.position.z = 0;
+				msg.pose.orientation.x = 0;
+				msg.pose.orientation.y = 0;
+				msg.pose.orientation.z = 0;
+				msg.pose.orientation.w = 0;
+				msg.speed = 10;
+				msg.estop = false;
+				msg.reset = false;
+				return msg;
+			}();
+      if (msg->axes[0] > 0.4)
+      {
+        poseCmd.pose.position.y = 1;
+      }
+      if (msg->axes[0] < -0.4)
+      {
+        poseCmd.pose.position.y = -1;
+      }
+      if (msg->axes[1] > 0.4)
+      {
+        poseCmd.pose.position.x = 1;
+      }
+      if (msg->axes[1] < -0.4)
+      {
+        poseCmd.pose.position.x = -1;
+      }
+      if (msg->buttons[9] == 1)
+      {
+        poseCmd.pose.position.z = 1;
+      }
+      if (msg->buttons[10] == 1)
+      {
+        poseCmd.pose.position.z = -1;
+      }
+      if (msg->axes[2] > 0.4)
+      {
+        poseCmd.pose.orientation.x = -1;
+      }
+      if (msg->axes[2] < -0.4)
+      {
+        poseCmd.pose.orientation.x = 1;
+      }
+      if (msg->axes[3] > 0.4)
+      {
+        poseCmd.pose.orientation.y = 1;
+      }
+      if (msg->axes[3] < -0.4)
+      {
+        poseCmd.pose.orientation.y = -1;
+      }
+      if (msg->axes[4] == -1)
+      {
+        poseCmd.pose.orientation.z = 1;
+      }
+      if (msg->axes[5] == -1)
+      {
+        poseCmd.pose.orientation.z = -1;
+      }
+      if (msg->buttons[3] == 1)
+      {
+        poseCmd.pose.orientation.w = 1;
+      }
+      if (msg->buttons[1] == 1)
+      {
+        poseCmd = []{
+					interfaces::msg::ArmCmd msg;
+					msg.pose.position.x = 0;
+					msg.pose.position.y = 0;
+					msg.pose.position.z = 0;
+					msg.pose.orientation.x = 0;
+					msg.pose.orientation.y = 0;
+					msg.pose.orientation.z = 0;
+					msg.pose.orientation.w = 0;
+					msg.speed = 10;
+					msg.estop = false;
+					msg.reset = true;
+					return msg;
+				}();
+        //shouldPub = true;
+      }
+      if (!isEqual(poseCmd, oldCmd))
+      {
+        /*RCLCPP_INFO(this->get_logger(), "A pose: %f %f %f %f %f %f %f %f %i %i",
+					oldCmd.pose.position.x,
+					oldCmd.pose.position.y,
+					oldCmd.pose.position.z,
+					oldCmd.pose.orientation.x,
+					oldCmd.pose.orientation.y,
+					oldCmd.pose.orientation.z,
+					oldCmd.pose.orientation.w,
+					oldCmd.speed,
+					int(oldCmd.estop),
+					int(oldCmd.reset));//
+				RCLCPP_INFO(this->get_logger(), "B pose: %f %f %f %f %f %f %f %f %i %i",
+					poseCmd.pose.position.x,
+					poseCmd.pose.position.y,
+					poseCmd.pose.position.z,
+					poseCmd.pose.orientation.x,
+					poseCmd.pose.orientation.y,
+					poseCmd.pose.orientation.z,
+					poseCmd.pose.orientation.w,
+					poseCmd.speed,
+					int(poseCmd.estop),
+					int(poseCmd.reset));//*/
+        oldCmd = poseCmd;
+        shouldPub = true;
+		    /*auto message = std_msgs::msg::String();
+		    message.data = cmd;
+		    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+		    publisher_->publish(message);*/
+		  }
+    }
+    
+    void publish_message()
+    {
+      if (shouldPub)
+      {
+		    shouldPub = false;
+		    RCLCPP_INFO(this->get_logger(), "Current pose: %f %f %f %f %f %f %f %f %i %i",
+					oldCmd.pose.position.x,
+					oldCmd.pose.position.y,
+					oldCmd.pose.position.z,
+					oldCmd.pose.orientation.x,
+					oldCmd.pose.orientation.y,
+					oldCmd.pose.orientation.z,
+					oldCmd.pose.orientation.w,
+					oldCmd.speed,
+					int(oldCmd.estop),
+					int(oldCmd.reset));//*/
+			  publisher_->publish(oldCmd);
+			}
+    }
+    
+};
 
 int main(int argc, char ** argv)
 {
--- src/keyboard_publisher/src/keyboard_publisher.cpp
+++ src/keyboard_publisher/src/keyboard_publisher.cpp
@@ -1,91 +1,110 @@
-#include "keyboard_publisher.h"
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include "std_msgs/msg/string.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include "interfaces/msg/arm_cmd.hpp"
 
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-MinimalPublisher::MinimalPublisher()
-: Node("minimal_publisher"), count_(0)
+class MinimalPublisher : public rclcpp::Node
 {
-  publisher_ = this->create_publisher<interfaces::msg::ArmCmd>("arm_base_commands", 10);
-  timer_ = this->create_wall_timer(
-  500ms, std::bind(&MinimalPublisher::timer_callback, this));//*/
-}
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<interfaces::msg::ArmCmd>("arm_base_commands", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));//*/
+    }
 
-void MinimalPublisher::timer_callback()
-{
-  interfaces::msg::ArmCmd poseCmd = []{
-		interfaces::msg::ArmCmd msg;
-		msg.pose.position.x = 0;
-		msg.pose.position.y = 0;
-		msg.pose.position.z = 0;
-		msg.pose.orientation.x = 0;
-		msg.pose.orientation.y = 0;
-		msg.pose.orientation.z = 0;
-		msg.pose.orientation.w = 0;
-		msg.speed = 10;
-		msg.estop = false;
-		msg.reset = false;
-		return msg;
-	}();
-  char c;
-  std::cin>>c;
-  if (c == 'w')
-  {
-    poseCmd.pose.position.x = 1;
-  }
-  else if (c == 's')
-  {
-    poseCmd.pose.position.x = -1;
-  }
-  else if (c == 'a')
-  {
-    poseCmd.pose.position.y = 1;
-  }
-  else if (c == 'd')
-  {
-    poseCmd.pose.position.y = -1;
-  }
-  else if (c == 'z')
-  {
-    poseCmd.pose.position.z = 1;
-  }
-  else if (c == 'x')
-  {
-    poseCmd.pose.position.z = -1;
-  }
-  else if (c == 'r')
-  {
-    poseCmd.pose.orientation.x = 1;
-  }
-  else if (c == 't')
-  {
-    poseCmd.pose.orientation.x = -1;
-  }
-  else if (c == 'f')
-  {
-    poseCmd.pose.orientation.y = 1;
-  }
-  else if (c == 'g')
-  {
-    poseCmd.pose.orientation.y = -1;
-  }
-  else if (c == 'c')
-  {
-    poseCmd.pose.orientation.z = 1;
-  }
-  else if (c == 'v')
-  {
-    poseCmd.pose.orientation.z = -1;
-  }
-  //auto message = std_msgs::msg::String();
-  //geometry_msgs::msg::Pose message;
-  //message.data = cmd;
-  //RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-  publisher_->publish(poseCmd);
-}
-
+  private:
+    void timer_callback()
+    {
+      interfaces::msg::ArmCmd poseCmd = []{
+				interfaces::msg::ArmCmd msg;
+				msg.pose.position.x = 0;
+				msg.pose.position.y = 0;
+				msg.pose.position.z = 0;
+				msg.pose.orientation.x = 0;
+				msg.pose.orientation.y = 0;
+				msg.pose.orientation.z = 0;
+				msg.pose.orientation.w = 0;
+				msg.speed = 10;
+				msg.estop = false;
+				msg.reset = false;
+				return msg;
+			}();
+      char c;
+      std::cin>>c;
+      if (c == 'w')
+      {
+        poseCmd.pose.position.x = 1;
+      }
+      else if (c == 's')
+      {
+        poseCmd.pose.position.x = -1;
+      }
+      else if (c == 'a')
+      {
+        poseCmd.pose.position.y = 1;
+      }
+      else if (c == 'd')
+      {
+        poseCmd.pose.position.y = -1;
+      }
+      else if (c == 'z')
+      {
+        poseCmd.pose.position.z = 1;
+      }
+      else if (c == 'x')
+      {
+        poseCmd.pose.position.z = -1;
+      }
+      else if (c == 'r')
+      {
+        poseCmd.pose.orientation.x = 1;
+      }
+      else if (c == 't')
+      {
+        poseCmd.pose.orientation.x = -1;
+      }
+      else if (c == 'f')
+      {
+        poseCmd.pose.orientation.y = 1;
+      }
+      else if (c == 'g')
+      {
+        poseCmd.pose.orientation.y = -1;
+      }
+      else if (c == 'c')
+      {
+        poseCmd.pose.orientation.z = 1;
+      }
+      else if (c == 'v')
+      {
+        poseCmd.pose.orientation.z = -1;
+      }
+      //auto message = std_msgs::msg::String();
+      //geometry_msgs::msg::Pose message;
+      //message.data = cmd;
+      //RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(poseCmd);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<interfaces::msg::ArmCmd>::SharedPtr publisher_;
+    size_t count_;
+};
 
 int main(int argc, char * argv[])
 {
