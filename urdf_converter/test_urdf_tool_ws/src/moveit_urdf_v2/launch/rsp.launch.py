b1ac75f44c27889c16b5004501a8725ff5ab384b
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-07-02 11:32:56 -0400

    Built new arm

---
+++ urdf_converter/test_urdf_tool_ws/src/moveit_urdf_v2/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("ArmURDF2", package_name="moveit_urdf_v2").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
