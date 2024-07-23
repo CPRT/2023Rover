10e667fa816d1602d7ebc7d474d11ac14b7c08eb
Erik <erikcaell@gmail.com>
2024-07-22 20:06:36 -0400

    Create nav_commanders package

---
+++ src/nav_commanders/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'nav_commanders'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='erik',
+    maintainer_email='erikcaell@gmail.com',
+    description='Python Simple Commanders to control high level nav2 logic',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
