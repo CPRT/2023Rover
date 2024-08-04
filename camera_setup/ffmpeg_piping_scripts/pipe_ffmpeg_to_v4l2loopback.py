f3bf35c85aadb6dcb859b10601a65540049439cb
lead <softlead@curovers.ca>
2024-08-03 22:39:58 -0400

    Update ffmpeg services on the jetson and install venv

--- camera_setup/ffmpeg_piping_scripts/pipe_ffmpeg_to_v4l2loopback.py
+++ camera_setup/ffmpeg_piping_scripts/pipe_ffmpeg_to_v4l2loopback.py
@@ -13,34 +13,36 @@ Steps to add a new camera
 """
 
 def get_input_output_indexes(camera_profile: str) -> Tuple[int, int]:
-    if camera_profile == 'ZED':
+    if camera_profile == 'ZED'.lower():
         return ZED()
-    elif camera_profile == 'ExpensiveAssLowLightCamera':
+    elif camera_profile == 'ExpensiveAssLowLightCamera'.lower():
         return ExpensiveAssLowLightCamera()    
-    elif camera_profile == 'IR_ELP':
+    elif camera_profile == 'IR_ELP'.lower():
         return IR_ELP()
-    elif camera_profile == 'PS3_EYE':
+    elif camera_profile == 'PS3_EYE'.lower():
         return PS3_EYE()
+    else:
+        exit_with_error(os.EX_USAGE, f"Unable to determine camera_profile in get_input_output_indexes")
 
 def ZED() -> Tuple[int, int]:
     output_index = 10
     camera_index = find_video_index_by_id("usb-Technologies__Inc._ZED_2i_OV0001-video-index0")
-    return (camera_index, output_index)
+    return camera_index, output_index
 
 def ExpensiveAssLowLightCamera() -> Tuple[int, int]:
     output_index = 11
     camera_index = find_video_index_by_id("usb-e-con_systems_See3CAM_CU27_3B1519112B010900-video-index0")
-    return (camera_index, output_index)
+    return camera_index, output_index
 
 def IR_ELP() -> Tuple[int, int]:
     output_index = 14
     camera_index = find_video_index_by_id("usb-Sonix_Technology_Co.__Ltd._USB_2.0_Camera_SN5100-video-index0")
-    return (camera_index, output_index)
+    return camera_index, output_index
 
 def PS3_EYE() -> Tuple[int, int]:
     output_index = 15
     camera_index = find_video_index_by_id("usb-OmniVision_Technologies__Inc._USB_Camera-B4.09.24.1-video-index0")
-    return (camera_index, output_index)
+    return camera_index, output_index
 
 
 def find_video_index_by_id(v4l_byid_name: str) -> int:
@@ -93,7 +95,11 @@ def main():
             else:
                 exit_with_error(os.EX_USAGE, f"Must provide the camera profile with --camera")
 
-        if selected_profile not in CAMERA_PROFILE_NAMES:
+        camera_options_lower = []
+        for cam in CAMERA_PROFILE_NAMES:
+            camera_options_lower.append(cam.lower())
+
+        if selected_profile not in camera_options_lower:
             exit_with_error(os.EX_USAGE, f"Invalid camera profile (provided: {selected_profile}). Options are {repr(CAMERA_PROFILE_NAMES)}")
 
     except Exception as e:
