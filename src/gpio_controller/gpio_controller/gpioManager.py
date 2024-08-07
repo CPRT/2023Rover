a576d5e12ae453449e58b62433c6b17d3dc985cb
Chris <christopherrusu1@gmail.com>
2024-08-07 13:08:15 -0400

    add test

--- src/gpio_controller/gpio_controller/gpioManager.py
+++ src/gpio_controller/gpio_controller/gpioManager.py
@@ -7,11 +7,12 @@ import Jetson.GPIO as GPIO
 import interfaces.msg as GPIOmsg
 
 
-class joystickDrive(Node):
+class gpioManager(Node):
     def __init__(self):
         super().__init__("gpioNode")
         self.lastTimestamp = 0
         mode = GPIO.getmode()
+        self.gpiooutput = False
         GPIO.setmode(mode)
         output_pins = {
             'JETSON_XAVIER': 18,
@@ -27,29 +28,41 @@ class joystickDrive(Node):
         if output_pin is None:
             raise Exception('PWM not supported on this board')
         
-        GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
-        p = GPIO.PWM(output_pin, 50)
-        val = 25
-        incr = 5
-        p.start(val)
+        # GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+        # p = GPIO.PWM(output_pin, 50)
+        # val = 25
+        # incr = 5
+        # p.start(val)
 
-        print("PWM running. Press CTRL+C to exit.")
-        try:
-            while True:
-                Sleep(0.25)
-                if val >= 100:
-                    incr = -incr
-                if val <= 0:
-                    incr = -incr
-                val += incr
-                p.ChangeDutyCycle(val)
-        finally:
-            p.stop()
-            GPIO.cleanup()
+        # print("PWM running. Press CTRL+C to exit.")
+        # try:
+        #     while True:
+        #         Sleep(0.25)
+        #         if val >= 100:
+        #             incr = -incr
+        #         if val <= 0:
+        #             incr = -incr
+        #         val += incr
+        #         p.ChangeDutyCycle(val)
+        # finally:
+        #     p.stop()
+        #     GPIO.cleanup()
 
-        channels = [18, 12, 13]
-        GPIO.setup(channels, GPIO.OUT)
-        GPIO.output(channels, 5)
+        channels = [32]
+        GPIO.setup(channels, GPIO.OUT, initial=GPIO.LOW)
+        # GPIO.output(channels, GPIO.HIGH)
+        freq = 1
+        self.rate = self.create_rate(freq)
+        period = 1 / freq
+        self.timer = self.create_timer(period, self.ledTimer)
+
+    def ledTimer(self):
+        if(self.gpiooutput):
+            GPIO.output(32, GPIO.HIGH)
+            self.gpiooutput = False
+        else:
+            GPIO.output(32, GPIO.LOW)
+            self.gpiooutput = True
 
 
 
