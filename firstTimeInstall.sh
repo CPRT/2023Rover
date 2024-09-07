a8fac114a1b9c132c6774d9f9c0748bea144eac6
ConnorNeed <129120300+ConnorNeed@users.noreply.github.com>
2024-09-07 12:59:27 -0400

    chore: first time install script

---
+++ firstTimeInstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install ros-humble-desktop
+
+sudo apt install ros-humble-ros-base
+
+sudo apt install ros-dev-tools
+
+echo source /opt/ros/humble/setup.bash >> ~.bashrc
+
+sudo apt install python3-dev
+
+sudo apt install python3-pip
+
+pip3 install black
+
+sudo apt install clang-format
+
+sudo apt install clang-tidy
+
