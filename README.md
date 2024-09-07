2ea20c2f20979d421eb40c58e2474ef022a06d4b
ConnorNeed <129120300+ConnorNeed@users.noreply.github.com>
2024-09-07 12:33:16 -0400

    chore: cleanup and add make.sh

--- README.md
+++ README.md
@@ -1,38 +1,15 @@
-git clone https://github.com/CPRT/2023Rover.git
+# CPRT Rover
+## Getting started
+1) Clone the repo
+    ```bash
+    git clone git@github.com:CPRT/2023Rover.git
+    ```
+2) Source ros2 humble (If not in .bashrc)
+    ```bash
+    source /opt/ros/humble/setup.bash
+    ```
+3) Build the packages
+    ```bash
+    ./make.sh
+    ```
 
-cd 2023Rover
-
-git checkout Arm-Simulation
-
-source /opt/ros/humble/setup.bash
-
-colcon build
-
-source install/setup.bash 
-
-ros2 launch rover_arm_config demo.launch.py
-
-takes maybe a minute or 2 to get the planner running, if you see a ton of green text saying its ready in the console try moving around the ball and pressing plan/ plan and execute
-
-
-
-Launch simple controller:
-
-Terminal 1:
-source ros and install
-ros2 launch test_urdf_v2 demo.launch.py
-
-terminal 2:
-ros2 run moveit_controller moveit_controller
-
-terminal 3:
-ros2 run keyboard_publisher keyboard_publisher
-Start entering w, a, s, or d and press enter
-z and x to go up and down
-r and t to twist wrist
-f and g to rotate wrist up and down
-c and v to rotate wrist side to side
-q to stop
-n to reset to original position
-b + 6 numbers to set angles in degrees from orange arm in rviz
-(Example command: b35 67 12 -42 5 1)
---
+++ make.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+# List of directories to exclude
+# Only exclude third party code
+excluded_dirs=("ros_phoenix_humble" "ouster-ros" "spatio_temporal_voxel_layer")
+
+# Function to find files excluding certain directories
+find_files_excluding_dirs() {
+    local extensions=("$@")
+    
+    # Remove the directory list from the extensions array
+    local ext_list=("${extensions[@]:0:${#extensions[@]}-1}")
+    local last_ext=${extensions[-1]}
+
+    # Build the find command dynamically
+    local find_cmd="find . -type d \( "
+    for dir in "${excluded_dirs[@]}"; do
+        find_cmd+=" -path ./$dir -o"
+    done
+
+    # Remove the trailing '-o'
+    find_cmd=${find_cmd%-o}
+
+    find_cmd+=" \) -prune -o \( "
+
+    # Build the name pattern for multiple extensions
+    local ext_pattern=""
+    for ext in "${ext_list[@]}"; do
+        ext_pattern+=" -name '*.$ext' -o"
+    done
+
+    # Remove the trailing '-o'
+    ext_pattern=${ext_pattern%-o}
+
+    find_cmd+="$ext_pattern \) -print"
+
+    # Execute the find command
+    eval "$find_cmd"
+}
+
+run_as_root() {
+  if [ "$EUID" -ne 0 ]; then
+    sudo "$@"
+  else
+    "$@"
+  fi
+}
+
+
+python_files=find_files_excluding_dirs "py"
+
+black $python_files
+
+cpp_files=find_files_excluding_dirs "h" "hpp" "cpp"
+
+clang-format -i -style=file $cpp_files
+
+run_as_root rosdep install --from-paths src -r -y
+
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --symlink-install
+
+clang-tidy -p ./build $cpp_files $INCLUDE_PATHS || exit 1
+
+source install/local_setup.bash
