e2c5976238e50f2e42d71aec0e445f5962a2e38b
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2024-11-29 09:38:32 -0500

    Template

--- src/rover_arm_config/launch/rsp.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_rsp_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("urdf_ARM-3000", package_name="rover_arm_config").to_moveit_configs()
-    return generate_rsp_launch(moveit_config)
